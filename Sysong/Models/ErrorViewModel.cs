f818668d70e8c650d4189177d4b125a4341dbe91
alephyv <alephyvinicius18@gmail.com>
2022-06-22 10:44:45 -0300

    doing home and navbar

---
+++ Sysong/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sysong.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Sysong/wwwroot/js/site.js
@@ -0,0 +1,70 @@
+﻿$(document).ready(function () {
+    M.updateTextFields();
+    $('.datepicker').datepicker({
+        format: 'dd/mm/yyyy',
+        i18n: {
+            cancel: 'Cancelar',
+            clear: 'Limpar',
+            done: 'Ok',
+            monthsShort: [
+                'Jan',
+                'Feb',
+                'Mar',
+                'Abr',
+                'Mai',
+                'Jun',
+                'Jul',
+                'Ago',
+                'Set',
+                'Out',
+                'Nov',
+                'Dez'
+            ],
+            months: [
+                'Janeiro',
+                'Fevereiro',
+                'Março',
+                'Abril',
+                'Maio',
+                'Junho',
+                'Julho',
+                'Agosto',
+                'Setembro',
+                'Outubro',
+                'Novembro',
+                'Dezembro'
+            ],
+            weekdays: [
+                'Domingo',
+                'Segunda-Feira',
+                'Terça-Feira',
+                'Quarta-Feira',
+                'Quinta-Feira',
+                'Sexta-Feira',
+                'Sábado'
+            ],
+            weekdaysShort: [
+                'Dom',
+                'Seg',
+                'Ter',
+                'Qua',
+                'Qui',
+                'Sex',
+                'Sáb'
+            ],
+            weekdaysAbbrev: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+        }
+    });
+    $('select').formSelect();
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.modal').modal();
+    $(".dropdown-trigger").dropdown({
+        hover: true,
+        alignment: 'right',
+        coverTrigger: false,
+        constrain_width: true,
+        focusedIndex: 2
+    });
+    $('.chips').chips();
+});
